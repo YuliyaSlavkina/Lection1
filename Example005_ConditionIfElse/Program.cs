a8889f22d90ce88731573f3f6995ae0eb38cc19b
Julia <wuwara@mail.ru>
2022-11-29 16:12:03 +0300

    Example007 Magic

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,18 @@
-﻿Console.Write ("Введите имя пользователя: ");
-string username = Console.ReadLine();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите имя пользователя: ");
+        string username = Console.ReadLine();
 
-if (username.ToLower() == "маша")
-{
-    Console.WriteLine("Ура, это же МАША");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
+        if (username.ToLower() == "маша")
+        {
+            Console.WriteLine("Ура, это же МАША");
+        }
+        else
+        {
+            Console.Write("Привет, ");
+            Console.WriteLine(username);
+        }
+    }
 }
