1ccc18902d6e35d90c74651cf6fd3d7de427b9ae
Julia <wuwara@mail.ru>
2022-12-01 18:27:35 +0300

    Example010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 18, 22, 35, 4, 56, 73, 22 };
+
+int n = array.Length;
+int find = 22;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
